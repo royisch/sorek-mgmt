ec26d59a3cf2c18df8053f0adc10d8f0f2131dc5
Royi <royi@bizzabo.com>
2016-03-14 23:42:36 +0200

    conenct to google drive

--- src/modules/drive.js
+++ src/modules/drive.js
@@ -1,6 +1,6 @@
 import googleDrive from 'google-drive'
 import passport from 'passport'
-
+var cookieName = 'sm-cookie';
 module.exports = {
   initGoogleAuth: function (app) {
 
@@ -16,48 +16,40 @@ module.exports = {
     passport.serializeUser(serializer);
     passport.deserializeUser(serializer);
 
-
-// Use the GOOGLEStrategy within Passport.
-//   Strategies in Passport require a `verify` function, which accept
-//   credentials (in this case, an accessToken, refreshToken, and GOOGLE
-//   profile), and invoke a callback with a user object.
     passport.use(new GoogleStrategy({
       callbackURL: 'http://localhost:3000/auth/google/return',
       clientID:GOOGLE_APP_ID,
       clientSecret:GOOGLE_APP_SECRET,
       passReqToCallback   : true
       },
-      function(accessToken, refreshToken, profile, done) {
-        if(!profile){
-          done(null, null);
-        }
-        //done(null,{token: accessToken, profile: currentUser});
-        done(null,currentUser);
+      function(request, accessToken, refreshToken, profile, done) {
+        profile.accessToken = accessToken;
+        profile.refreshToken = refreshToken;
+        process.nextTick(function () {
+          return done(null, profile);
+        });
       })
-
     );
 
+    app.get('/auth/google',passport.authenticate('google', {
+      scope: ['https://www.googleapis.com/auth/userinfo.profile',
+        'https://www.googleapis.com/auth/drive',
+        'https://www.googleapis.com/auth/drive.appfolder',
+        'https://www.googleapis.com/auth/drive.file']
+    }));
 
-
-// GET /auth/facebook
-//   Use passport.authenticate() as route middleware to authenticate the
-//   request.  The first step in Facebook authentication will involve
-//   redirecting the user to facebook.com.  After authorization, Facebook will
-//   redirect the user back to this application at /auth/facebook/callback
-    app.get('/auth/google',passport.authenticate('google', { scope: ['https://www.googleapis.com/auth/drive',
-      'https://www.googleapis.com/auth/drive.file'] })
-    );
-
-// GET /auth/facebook/callback
-//   Use passport.authenticate() as route middleware to authenticate the
-//   request.  If authentication fails, the user will be redirected back to the
-//   login page.  Otherwise, the primary route function function will be called,
-//   which, in this example, will redirect the user to the home page.
     app.get('/auth/google/return',
       passport.authenticate('google', { failureRedirect: '/' }),
       function(req, res) {
         // Successful authentication, redirect home.
+        console.log(req.cookies[cookieName]);
         res.redirect('/listDocs');
       });
+
+    app.get('getDocs', function(req, res) {
+      console.log(req.cookies[cookieName]);
+      res.send({'puta': 'madre'});
+    });
+
   }
 };
